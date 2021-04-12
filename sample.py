9ff1abf227bbd10146404f71f75d8d0c56008479
kr-g <k.r.goger@gmail.com>
2021-04-12 12:49:01 +0200

    added resolve to sudoko example

--- sample.py
+++ sample.py
@@ -1,34 +1,37 @@
-
 from dataflow import CellDataFlow, print_error, clear_error
 
 
 cf = CellDataFlow()
 
-c1 = cf.create_cell() # or shorthand -> c1 = cf()
+c1 = cf.create_cell()  # or shorthand -> c1 = cf()
 
-#lambda function is called with cell,cell.val
-c2 = cf(watching=c1, func=lambda c,v: v-1 )
-c3 = cf(watching=[c1], func=lambda c,v: v*2 )
+# lambda function is called with cell,cell.val
+c2 = cf(watching=c1, func=lambda c, v: v - 1)
+c3 = cf(watching=[c1], func=lambda c, v: v * 2)
 
 # since watching to 2 cells its not predictable what c,v is used in the call
 # could be any of the watches, c2 or c3
-c4 = cf(watching=[c2,c3], func= lambda c,v: c2.val+c3.val )
+c4 = cf(watching=[c2, c3], func=lambda c, v: c2.val + c3.val)
 
-c5 = cf(watching=[c4], id="c5", func=lambda c,v : c2.val+c3.val+c4.val )
+c5 = cf(watching=[c4], id="c5", func=lambda c, v: c2.val + c3.val + c4.val)
 
 
 # new in version v0.0.4, ref and lazy bind
 
 # lazy bound cells are referenced by id rather then object instance
 # create a lazy bound cells _before_ cell c5clone is created
-c5_lazy = cf(lazy_watching="c5clone", func=lambda c,v : str(v) + "*" )
+c5_lazy = cf(lazy_watching="c5clone", func=lambda c, v: str(v) + "*")
 
 # refer to c5clone with ref() function by id
-c5ref_lazy = cf(lazy_watching="c5clone", func=lambda c,v : str(c.ref("c5clone").val) + "!!" )
-c5ref_lazy_array = cf(lazy_watching=["c5clone"], func=lambda c,v : str(c.ref("c5clone").val) + "!!!" )
-c5lazy_array = cf(lazy_watching=["c5clone"], func=lambda c,v : str(v) + "<-" )
+c5ref_lazy = cf(
+    lazy_watching="c5clone", func=lambda c, v: str(c.ref("c5clone").val) + "!!"
+)
+c5ref_lazy_array = cf(
+    lazy_watching=["c5clone"], func=lambda c, v: str(c.ref("c5clone").val) + "!!!"
+)
+c5lazy_array = cf(lazy_watching=["c5clone"], func=lambda c, v: str(v) + "<-")
 
-c5_the_clone = cf( id="c5clone", watching=[c5], func=lambda c,v : str(v) + "!" )
+c5_the_clone = cf(id="c5clone", watching=[c5], func=lambda c, v: str(v) + "!")
 
 # dont forget to bind all lazy watches before starting the dataflow
 cf.bind()
@@ -37,65 +40,71 @@ cf.bind()
 # use find to add all depending watches automatically
 # print output will show that the data goes directly forward
 # instead of arriving in one of the next round(s)
-c6 = cf(watching=cf.find([c2,c5]), func=lambda c,v : c2.val + c5.val )
+c6 = cf(watching=cf.find([c2, c5]), func=lambda c, v: c2.val + c5.val)
 
-def cust_func(c,v):
+
+def cust_func(c, v):
     # print(f"do something usefull because of {v}")
-    return v*3.14
+    return v * 3.14
 
-c7 = cf(watching=c6, func=cust_func )
+
+c7 = cf(watching=c6, func=cust_func)
 # shorthand for cf.find
-c8 = cf(watching=cf(c6), func=lambda c,v : v*3.14 )
+c8 = cf(watching=cf(c6), func=lambda c, v: v * 3.14)
 
-def sumup(c,v):    
-    total = sum(map( lambda x : x.val, c.watching))
+
+def sumup(c, v):
+    total = sum(map(lambda x: x.val, c.watching))
     return total
-    
-c9 = cf(watching=cf(c8), func=sumup )
+
+
+c9 = cf(watching=cf(c8), func=sumup)
 
 # no func provided, just get the data from the cell here
 # add print_error to see difference in output
-c10 = cf(id="c10", watching=c6, func=lambda c,v : 1/0 , err=None )
+c10 = cf(id="c10", watching=c6, func=lambda c, v: 1 / 0, err=None)
 
 # err function is called when func fails
 # called as errfunc( cell, val, ex )
 # use clear_error for clean up
-c11 = cf(id="c11", watching=c6, err=None )
+c11 = cf(id="c11", watching=c6, err=None)
 
 
-def strcats(c,v):
+def strcats(c, v):
     # watching is a set, so the result combination is unpredictable
-    return " ".join(map( lambda x : x.val , c.watching))
-    
+    return " ".join(map(lambda x: x.val, c.watching))
+
+
 cs1 = cf()
 cs2 = cf()
-cs3 = cf(watching=[cs1,cs2], func=strcats )
+cs3 = cf(watching=[cs1, cs2], func=strcats)
 
-cs4 = cf(watching=[cs1,cs2], func=lambda c,v : (cs1.val + cs2.val).upper() )
+cs4 = cf(watching=[cs1, cs2], func=lambda c, v: (cs1.val + cs2.val).upper())
 
 # add some custom data in case val is not sufficient
 cs4.meta["extra_data"] = "meta dict can be used for own purposes"
 
 # this will later removed from the flow
-cdrop = cf(watching=c6 )
+cdrop = cf(watching=c6)
 
 # assignment only after flow is defined
 # assignment triggers later processing
 c1.val = 3
-cs1.val ="hi"
-cs2.val ="ya"
+cs1.val = "hi"
+cs2.val = "ya"
 
 
 def val(x):
     if x.error:
         return "#ERR#"
     try:
-        return round(x.val,2)
+        return round(x.val, 2)
     except:
         return x.val
 
+
 for cell in cf.cells:
-    print(val(cell),end="\t")
+    print(val(cell), end="\t")
 print()
 
 # propagate pushes data to the next depending cells
@@ -105,34 +114,36 @@ print()
 # since there is no circle defined its possible to loop
 while cf.propagate():
     for cell in cf.cells:
-        print(val(cell),end="\t")
+        print(val(cell), end="\t")
     print()
 
 
 print("drop cell")
-cf.drop_cell( cdrop ) # remove from the flow definition
+cf.drop_cell(cdrop)  # remove from the flow definition
 
 
 print("recalc with c1=4, change greeting, cdrop will keep old val!")
 c1.val = 4
 cs2.val = "you"
 
+
 def printit():
     for c in cf.cells:
-        print(val(c),end="\t")
+        print(val(c), end="\t")
     print(cdrop.val)
 
-# use the loop which calls propagate 
+
+# use the loop which calls propagate
 # max calls is number of cells involed (runs parameter set to default:-1)
 # call func after every propagate
-cf.loop( func=printit, runs=-1 )
+cf.loop(func=printit, runs=-1)
 
 print("--- same val---")
 c1.val = 4
 
 # nothing happens here since val is unchanged
 runs = cf.loop(func=printit)
-print( "runs", runs )
+print("runs", runs)
 
 printit()
 
--- sample_sudoko.py
+++ sample_sudoko.py
@@ -12,158 +12,175 @@ getg, for getting one cells value
 setb, for setting one cells value, returns the notified cells
 undo, revert last move
 
+or call
+resolve, to solve the game
+
 """
 
 from dataflow import CellDataFlow, print_error, clear_error
 from collections import deque
+import random
 
 
-def getid(x,y):
+def getid(x, y):
     return f"{x},{y}"
 
-def create_board(cf,type):
+
+def create_board(cf, type):
     dim = type * type
     cells = dim * dim
     board = []
-    for x in range(0,dim):
-        for y in range(0,dim):
-            c = cf(id = getid(x,y))
-            c.meta["x"]=x
-            c.meta["y"]=y
-            c.meta["dim"]=dim
-            c.meta["type"]=type
+    for x in range(0, dim):
+        for y in range(0, dim):
+            c = cf(id=getid(x, y))
+            c.meta["x"] = x
+            c.meta["y"] = y
+            c.meta["dim"] = dim
+            c.meta["type"] = type
             board.append(c)
     return board
+
 
 def dim_board(board):
     dim = board[0].meta["dim"]
     cells = dim * dim
     return dim, cells
 
-def init_board(cf,board):
-    dim , cells = dim_board(board)
+
+def init_board(cf, board):
+    dim, cells = dim_board(board)
     type = board[0].meta["type"]
-    
-    for x in range(0,dim):
-        for y in range(0,dim):
+
+    for x in range(0, dim):
+        for y in range(0, dim):
             watch = set()
-            for i in range(0,dim):
+            for i in range(0, dim):
                 # row cells
-                watch.add( getid( i, y ) )
+                watch.add(getid(i, y))
                 # col cells
-                watch.add( getid( x, i ) )
-            # block cells     
+                watch.add(getid(x, i))
+            # block cells
             xb = int(x / type) * type
             yb = int(y / type) * type
-            for xi in range( xb, xb+type ):
-                for yi in range( yb, yb+type ):
-                    watch.add( getid( xi, yi ) )
-            #remove self reference
-            selfid = getid(x,y)
+            for xi in range(xb, xb + type):
+                for yi in range(yb, yb + type):
+                    watch.add(getid(xi, yi))
+            # remove self reference
+            selfid = getid(x, y)
             watch.remove(selfid)
             # find myself ;-)
             cell = cf.ids[selfid]
             watching = set()
             for w in watch:
-                cell.watches( cf.ids[w] )
+                cell.watches(cf.ids[w])
             cell.meta["watch"] = watch
             cell.meta["watching"] = watching
-                                
-            def before(c,v):
-                
+
+            def before(c, v):
+
                 # print the cell id
-                print( c.id, end=" " )
-                
+                # print( c.id, end=" " )
+
                 # update hints
                 c.hints(c)
                 # return false to stop data flow
                 return False
-            
+
             def hints(self):
-                constrained = set(map( lambda x: x.val, self.watching ))
-                h = set(range(1,10)).difference(constrained)
+                constrained = set(map(lambda x: x.val, self.watching))
+                h = set(range(1, 10)).difference(constrained)
                 self.meta["hints"] = h
                 return h
-                
+
             cell.hints = hints
             cell.before = before
-            
+
 
 def sanitize(data):
-    for s in [ " ", "\t", "\n", "\r" ]:
-        data = data.replace(s,"")    
-    data = data.replace(".","0")
+    for s in [" ", "\t", "\n", "\r"]:
+        data = data.replace(s, "")
+    data = data.replace(".", "0")
     return data
 
-def fill_board(cf,board,data):
-    dim , cells = dim_board(board)
+
+def fill_board(cf, board, data):
+    dim, cells = dim_board(board)
     inp = sanitize(data)
-    if len(inp)!=cells:
+    if len(inp) != cells:
         raise Exception(f"input does not match dimension {dim},{cells},{len(inp)}")
-    i = 0 
+    i = 0
     for c in inp:
         c = int(c)
         if c > 0:
             board[i].val = c
-            board[i].meta["preset"] = True
-        i+=1
+        board[i].meta["preset"] = c > 0
+        i += 1
 
-def print_board(cf,board):
+
+def print_board(cf, board):
     dim, cells = dim_board(board)
-    print( "\t", end=" " )
-    for h in range(0,dim):
-        print( h, end="\t" )
+    print("\t", end=" ")
+    for h in range(0, dim):
+        print(h, end="\t")
     print()
-    for y in range(0,dim):
-        print( y, end="\t" )
-        for x in range(0,dim):
-            cell = cf.ids[getid(x,y)]
-            v = cell._val            
+    for h in range(0, dim + 1):
+        print("-" * 4, end="\t")
+    print()
+    for x in range(0, dim):
+        print(str(x) + "  |", end="\t")
+        for y in range(0, dim):
+            cell = cf.ids[getid(x, y)]
+            v = cell._val
             if v is None:
                 v = " "
             else:
                 v = str(v)
-            print( v, end="\t" )
+            print(v, end="\t")
         print()
 
 
-stack = deque()        
+stack = deque()
 
-def getcell(cf,x,y):
-    return cf.ids[getid(x,y)]
 
-def getb(cf,x,y):
-    return getcell(cf,x,y).val
+def getcell(cf, x, y):
+    return cf.ids[getid(x, y)]
 
-def setb(cf,x,y,v):
-    cell = getcell(cf,x,y)
-    if "preset" in cell.meta:
+
+def getb(cf, x, y):
+    return getcell(cf, x, y).val
+
+
+def setb(cf, x, y, v):
+    cell = getcell(cf, x, y)
+    if cell.meta["preset"]:
         print("cant overwrite preset cell")
         return
     if v not in cell.meta["hints"]:
-        print("not possible")
+        print("not possible", x, y)
         return
     cell.val = v
     stack.append(cell)
     cnt = cf.loop()
     return cnt
-    
+
+
 def undo(cf):
     cell = stack.pop()
     cell.val = None
     cnt = cf.loop()
     return cnt
 
-def peek(cf,x,y):
-    cell = getcell(cf,x,y)
+
+def peek(cf, x, y):
+    cell = getcell(cf, x, y)
     if cell.val:
         return f"has val {cell.val}"
     return cell.meta["hints"]
 
 
 cf = CellDataFlow()
-board = create_board(cf,3)
-init_board( cf, board )
+board = create_board(cf, 3)
+init_board(cf, board)
 
 
 game = """      
@@ -178,28 +195,82 @@ game = """
     000 500 610
 """
 
-fill_board( cf, board, game )
+fill_board(cf, board, game)
 
 # set up the game
 for c in cf.cells:
     c.reset_trigger()
     c.hints(c)
 
-# this down nothing...
+# this will trigger no action here...
 cnt = cf.loop()
 
-print_board(cf,board)
+print_board(cf, board)
 
 
 def sample_calls_like():
-    getcell(cf,0,0)
-    getb(cf,0,0)
-    peek(cf,0,0)
-    setb(cf,0,0,1)
+    getcell(cf, 0, 0)
+    getb(cf, 0, 0)
+    peek(cf, 0, 0)
+    setb(cf, 0, 0, 1)
     print()
-    print_board(cf,board)
+    print_board(cf, board)
     undo(cf)
     print()
-    print_board(cf,board)
+    print_board(cf, board)
 
 
+def resolve(debug=False):
+    """call resolve to solve the game"""
+    cells = filter(lambda x: x.meta["preset"] == False, cf.cells)
+    rc = _resolve(cells, debug=debug)
+    if rc:
+        print_board(cf, board)
+    else:
+        print("no solution")
+
+
+cnt = 0
+
+
+def _resolve(cells=None, lvl=0, debug=False, shuffle=True):
+    global cnt
+    cnt += 1
+
+    cells = list(filter(lambda x: x.val == None or x.val == 0, cells))
+    if len(cells) == 0:
+        return True
+    # try this several times with debug=True
+    # to see that this solving strategy works better on average
+    if shuffle:
+        random.shuffle(cells)
+    # solve cells with less hints first
+    cells = sorted(cells, key=lambda x: len(x.meta["hints"]))
+
+    for c in cells:
+        x = c.meta["x"]
+        y = c.meta["y"]
+        all_hints = peek(cf, x, y)
+        if len(all_hints) == 0:
+            return False
+
+        for h in all_hints:
+            debug and print(
+                "set", x, y, "=", h, "lvl", lvl, "loops", cnt, "remaining", len(cells)
+            )
+            setb(cf, x, y, h)
+            result = _resolve(cells, lvl=lvl + 1, shuffle=shuffle, debug=debug)
+            if result:
+                return True
+            undo(cf)
+        return False
+    return False
+
+
+def print_hints():
+    cells = cf.cells
+    cells = filter(lambda x: x.meta["preset"] == False, cells)
+    cells = filter(lambda x: x.val == None or x.val == 0, cells)
+    cells = sorted(cells, key=lambda x: len(x.meta["hints"]))
+    for c in cells:
+        print(c.id, c.meta["hints"], len(c.meta["hints"]))
